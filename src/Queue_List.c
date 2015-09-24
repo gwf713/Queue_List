5e00ca9c093d1a71b91fd7dfbfb4d67b04663df5
Ge weifeng <gwf713@163.com>
2015-09-24 10:18:49 +0800

    Correct word in Queue_List.h and Queue_List.c

--- src/Queue_List.c
+++ src/Queue_List.c
@@ -1,18 +1,18 @@
 /******************************************************
- *		Queue_List.c
- * @file	Queue_List.c
- * @brief	Source file of list queue module
- * @version	0.0.2
+ *          Queue_List.c
+ * @file    Queue_List.c
+ * @brief   Source file of list queue module
+ * @version 0.0.2
  * @date    09-September-2015
- * @author	Gwf
+ * @author  Gwf
  *
  * Copyright(c) 2015, Gwf
  * All rights reserved
  *******************************************************/
-#include	"stdio.h"
-#include	"stdlib.h"
-#include  "string.h"
-#include	"Queue_List.h"
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+#include "Queue_List.h"
  
 /********** PRIVATE FUNCTION **********/
 static void Copy_To_Node(ps_Item pItem, ps_Node pNode, size_t Size_Item);
@@ -27,16 +27,16 @@ static void Init_Queue(ps_Queue	pq, size_t Size_Item);
  */
 ps_Queue Create_Queue(size_t Size_Item)
 {
-	ps_Queue pnew;
+    ps_Queue pnew;
 
-	pnew = (ps_Queue)malloc(sizeof(ts_Queue));
+    pnew = (ps_Queue)malloc(sizeof(ts_Queue));
 
-	if(pnew != NULL)
-	{
-		Init_Queue(pnew, Size_Item);
-	}
+    if(pnew != NULL)
+    {
+        Init_Queue(pnew, Size_Item);
+    }
 
-	return(pnew);
+    return(pnew);
 }
 
 /**
@@ -46,9 +46,9 @@ ps_Queue Create_Queue(size_t Size_Item)
  */
 void Delete_Queue(ps_Queue 	pq)
 {
-	Empty_Queue(pq);
+    Empty_Queue(pq);
 
-	free(pq);
+    free(pq);
 }
 
 /**
@@ -58,7 +58,7 @@ void Delete_Queue(ps_Queue 	pq)
  */
 FlagStatus Queue_Is_Full(const ps_Queue	pq)
 {
-	return((pq ->Count_Node >= LIMIT_QUEUE) ? SET : RESET);
+    return((pq ->Count_Node >= LIMIT_QUEUE) ? SET : RESET);
 }
 
 /**
@@ -68,7 +68,7 @@ FlagStatus Queue_Is_Full(const ps_Queue	pq)
  */
 FlagStatus Queue_Is_Empty(const ps_Queue	pq)
 {
-	return((pq ->Count_Node == 0) ? SET : RESET);
+    return((pq ->Count_Node == 0) ? SET : RESET);
 }
 
 /**
@@ -78,7 +78,7 @@ FlagStatus Queue_Is_Empty(const ps_Queue	pq)
  */
 uint8_t	Queue_Count(const ps_Queue	pq)
 {
-	return(pq ->Count_Node);
+    return(pq ->Count_Node);
 }
 
 /**
@@ -89,72 +89,72 @@ uint8_t	Queue_Count(const ps_Queue	pq)
  */
 ErrorStatus Queue_Write(ps_Item pItem, ps_Queue	pq)
 {
-	ps_Item pNewItem = NULL;
-	ps_Node pnew = NULL;
-	
-	if(pq ->Available == SET)
-	{
-		/* Disable queue module */
-		pq ->Available = RESET;
-		
-		/* If the queue is full, then return false. */
-		if(Queue_Is_Full(pq))
-		{
-			pq ->Available = SET;
-			return ERROR;
-		}
-			
-		pNewItem = (ps_Item)malloc(pq ->Size_Item);
-		
-		if(pNewItem == NULL)
-		{
-			pq ->Available = SET;
-			return ERROR;
-		}
-		
-		/* Allocate memory for new node. If no memory is available for node, return false. */
-		pnew = (ps_Node)malloc(sizeof(ts_Node));
-		
-		if(pnew == NULL)
-		{
-			free(pNewItem);
-			pq ->Available = SET;
-			return	ERROR;
-		}
-		else
-		{
-			pnew ->pItem = pNewItem;
-		}
-		
-		/* Copy item to node. */
-		Copy_To_Node(pItem, pnew, pq ->Size_Item);
-		
-		/* Insert new node to the queue. */
-		pnew ->pre = NULL;
+    ps_Item pNewItem = NULL;
+    ps_Node pnew = NULL;
 
-		if(Queue_Is_Empty(pq))
-		{
-			pq ->Tail = pnew;
-			pnew ->next = NULL;
-		}
-		else
-		{
-			pq ->Header ->pre = pnew;
-			pnew ->next = pq ->Header;
-		}
-		
-		pq ->Header = pnew;
-		pq ->Count_Node++;
-		
-		/* Enable queue module */
-		pq ->Available = SET;
-		
-		return SUCCESS;
-	}
-	else
-	{
-		return ERROR;
-	}
+    if(pq ->Available == SET)
+    {
+        /* Disable queue module */
+        pq ->Available = RESET;
+
+        /* If the queue is full, then return false. */
+        if(Queue_Is_Full(pq))
+        {
+            pq ->Available = SET;
+            return ERROR;
+        }
+
+        pNewItem = (ps_Item)malloc(pq ->Size_Item);
+
+        if(pNewItem == NULL)
+        {
+            pq ->Available = SET;
+            return ERROR;
+        }
+
+        /* Allocate memory for new node. If no memory is available for node, return false. */
+        pnew = (ps_Node)malloc(sizeof(ts_Node));
+
+        if(pnew == NULL)
+        {
+            free(pNewItem);
+            pq ->Available = SET;
+            return	ERROR;
+        }
+        else
+        {
+            pnew ->pItem = pNewItem;
+        }
+
+        /* Copy item to node. */
+        Copy_To_Node(pItem, pnew, pq ->Size_Item);
+
+        /* Insert new node to the queue. */
+        pnew ->pre = NULL;
+
+        if(Queue_Is_Empty(pq))
+        {
+            pq ->Tail = pnew;
+            pnew ->next = NULL;
+        }
+        else
+        {
+            pq ->Header ->pre = pnew;
+            pnew ->next = pq ->Header;
+        }
+
+        pq ->Header = pnew;
+        pq ->Count_Node++;
+
+        /* Enable queue module */
+        pq ->Available = SET;
+
+        return SUCCESS;
+    }
+    else
+    {
+        return ERROR;
+    }
 }
 
 /**
@@ -165,46 +165,46 @@ ErrorStatus Queue_Write(ps_Item pItem, ps_Queue	pq)
  */
 ErrorStatus Queue_Read(ps_Item pitem, ps_Queue pq)
 {
-	ps_Node pt;
-	
-	if(pq ->Available == SET)
-	{
-		pq ->Available = RESET;
+    ps_Node pt;
 
-		/* Check if queue is empty. Return false, if Empty. */
-		if(Queue_Is_Empty(pq))
-		{
-			pq ->Available = SET;
-			return ERROR;
-		}
+    if(pq ->Available == SET)
+    {
+        pq ->Available = RESET;
 
-		/* Copy item to the destination item. */
-		Copy_To_Item(pq ->Tail, pitem, pq ->Size_Item);
-	
-		/* The pre node is the new tail node, and free the read node memory. */
-		pt = pq ->Tail;
-		free(pt ->pItem);
-		free(pt);
-	
-		pq ->Tail = pq ->Tail ->pre;
-		if((pq ->Tail) != NULL)
-			pq ->Tail ->next = NULL;/* If the pq ->Tail is Null, what will happen? */
-	
-		/* Reduce the count of nodes. */
-		pq ->Count_Node--;
-	
-		/* If the count of nodes, then clear the header pointer of the queue. */
-		if(pq ->Count_Node == 0)
-			pq ->Header = NULL;
-	
-		pq ->Available = SET;
+        /* Check if queue is empty. Return false, if Empty. */
+        if(Queue_Is_Empty(pq))
+        {
+            pq ->Available = SET;
+            return ERROR;
+        }
 
-		return SUCCESS;
-	}
-	else
-	{
-		return ERROR;
-	}
+        /* Copy item to the destination item. */
+        Copy_To_Item(pq ->Tail, pitem, pq ->Size_Item);
+
+        /* The pre node is the new tail node, and free the read node memory. */
+        pt = pq ->Tail;
+        free(pt ->pItem);
+        free(pt);
+
+        pq ->Tail = pq ->Tail ->pre;
+        if((pq ->Tail) != NULL)
+            pq ->Tail ->next = NULL;/* If the pq ->Tail is Null, what will happen? */
+
+        /* Reduce the count of nodes. */
+        pq ->Count_Node--;
+
+        /* If the count of nodes, then clear the header pointer of the queue. */
+        if(pq ->Count_Node == 0)
+            pq ->Header = NULL;
+
+        pq ->Available = SET;
+
+        return SUCCESS;
+    }
+    else
+    {
+        return ERROR;
+    }
 }
 
 /**
@@ -214,87 +214,87 @@ ErrorStatus Queue_Read(ps_Item pitem, ps_Queue pq)
  */
 void Empty_Queue(ps_Queue pq)
 {
-	ps_Item dummy;
-	
-	dummy = (ps_Item)malloc(pq ->Size_Item);
-	
-	if(dummy == NULL)
-		return;
-	
-	/*Clear the nodes with functions "Queue_Read()"*/
-	while(!Queue_Is_Empty(pq))
-		Queue_Read(dummy, pq);
+    ps_Item dummy;
+
+    dummy = (ps_Item)malloc(pq ->Size_Item);
+
+    if(dummy == NULL)
+        return;
+
+    /*Clear the nodes with functions "Queue_Read()"*/
+    while(!Queue_Is_Empty(pq))
+        Queue_Read(dummy, pq);
 }
 
 #ifdef Node_Remove_EN
-	/**
-	 * @brief  Remove the node with item as same as parameter of item
-	 * @param  pq(ps_queue)
-	 * @param  item(ts_Item)
-	 * @retval None
-	 */
-	ErrorStatus Node_Remove(ps_Queue pq, ps_Node pnode)
+    /**
+     * @brief  Remove the node with item as same as parameter of item
+     * @param  pq(ps_queue)
+     * @param  item(ts_Item)
+     * @retval None
+     */
+    ErrorStatus Node_Remove(ps_Queue pq, ps_Node pnode)
     {
         if(pq ->Available == SET)
         {
             pq ->Available = RESET;
 
-			if(pq ->Header == pnode)
-			{
-			    pq ->Header = pnode ->next;
-			}
-			
-			if(pq ->Tail == pnode)
-			{
-			    pq ->Tail = pnode ->pre;
-			}
-			
-			/*Delete the node from queue*/
+            if(pq ->Header == pnode)
+            {
+                pq ->Header = pnode ->next;
+            }
+
+            if(pq ->Tail == pnode)
+            {
+                pq ->Tail = pnode ->pre;
+            }
+
+            /*Delete the node from queue*/
             if(pnode ->pre != NULL)
                 pnode ->pre ->next = pnode ->next;/* If there is no previous node, what will happen? */
             if(pnode ->next != NULL)
                 pnode ->next ->pre = pnode ->pre;/* If there is no next node, what will happen? */
 
-			
+
             free(pnode ->pItem);
             free(pnode);
 
             (pq ->Count_Node)--;
-	
+
             pq ->Available = SET;
 
             return(SUCCESS);
-		}
-		else
-		{
+        }
+        else
+        {
             return(ERROR);
-		}
+        }
     }
-	
-	/**
-	 * @brief  Find node in the queue, compare in pfun, 
-	 *         return the point to the node
-	 * @param  pq(ps_queue)
-	 * @param  pfun: compare the node in this function
-	 * @param  pfun(ps_Item)
-	 * @retval None
-	 */
-	ps_Node Queue_Find_Node(const ps_Queue pq, FlagStatus (*pfun)(const ps_Node, const ps_Item), const ps_Item pItem)
+
+    /**
+     * @brief  Find node in the queue, compare in pfun, 
+     *         return the point to the node
+     * @param  pq(ps_queue)
+     * @param  pfun: compare the node in this function
+     * @param  pfun(ps_Item)
+     * @retval None
+     */
+    ps_Node Queue_Find_Node(const ps_Queue pq, FlagStatus (*pfun)(const ps_Node, const ps_Item), const ps_Item pItem)
     {
-	    ps_Node pnode = pq ->Header;
-		
+        ps_Node pnode = pq ->Header;
+
         if(pnode != NULL)
-		{
+        {
             if(pfun(pnode, pItem) == SET)
-			{
-				return pnode;
-			}
-			else
-			{
-				pnode = pnode ->next;
-			}
-			
-			while(pnode != NULL)
+            {
+                return pnode;
+            }
+            else
+            {
+                pnode = pnode ->next;
+            }
+
+            while(pnode != NULL)
             {
                 if(pfun(pnode, pItem) == SET)
                 {
@@ -306,7 +306,7 @@ void Empty_Queue(ps_Queue pq)
                 }
             }
         }
-		
+
         return pnode;
     }
 #endif /* Node_Remove_EN */
@@ -319,15 +319,15 @@ void Empty_Queue(ps_Queue pq)
  */
 void Queue_Traverse(ps_Queue	pq,	ErrorStatus (*pfun)(ps_Node pNode, ps_Item pItem, ps_Queue pq), ps_Item pItem)
 {
-	ps_Node pnode = pq ->Header;
-	ErrorStatus Result_Function = ERROR;
-	
-	while(pnode ->next != NULL)
-	{
-		Result_Function = (* pfun)(pnode, pItem, pq);
-		Result_Function = Result_Function;
-		pnode = pnode ->next;
-	}
+    ps_Node pnode = pq ->Header;
+    ErrorStatus Result_Function = ERROR;
+
+    while(pnode ->next != NULL)
+    {
+        Result_Function = (* pfun)(pnode, pItem, pq);
+        Result_Function = Result_Function;
+        pnode = pnode ->next;
+    }
 }
 
 /****************	PRIVATE FUNCTION	********************/ 
@@ -338,10 +338,10 @@ void Queue_Traverse(ps_Queue	pq,	ErrorStatus (*pfun)(ps_Node pNode, ps_Item pIte
  */
 static void Init_Queue(ps_Queue	pq, size_t Size_Item)
 {
-	pq ->Header = pq	->Tail = NULL;
-	pq ->Count_Node = 0;
-	pq ->Size_Item = Size_Item;
-	pq ->Available = SET;
+    pq ->Header = pq	->Tail = NULL;
+    pq ->Count_Node = 0;
+    pq ->Size_Item = Size_Item;
+    pq ->Available = SET;
 }
 
 /**
@@ -352,7 +352,7 @@ static void Init_Queue(ps_Queue	pq, size_t Size_Item)
  */
 static void Copy_To_Node(ps_Item pItem, ps_Node pNode, size_t Size_Item)
 {
-	memcpy(pNode ->pItem, pItem, Size_Item);
+    memcpy(pNode ->pItem, pItem, Size_Item);
 }
 
 /**
@@ -363,5 +363,5 @@ static void Copy_To_Node(ps_Item pItem, ps_Node pNode, size_t Size_Item)
  */
 static void Copy_To_Item(ps_Node pNode, ps_Item pItem, size_t Size_Item)
 {
-	memcpy(pItem, pNode ->pItem, Size_Item);
+    memcpy(pItem, pNode ->pItem, Size_Item);
 }
